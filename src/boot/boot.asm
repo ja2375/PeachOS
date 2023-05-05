02a81cd2bc6d9f99d3966af4a2b4c821a2fd4873
Javi Hurtado <webmaster@javi-h.com>
2023-05-05 17:04:44 +0200

    Create a simple linker to link kernel and boot sector together

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -21,7 +21,7 @@ start:
     or eax, 0x1
     mov cr0, eax
     jmp CODE_SEG:load32
-
+    
 ; GDT
 gdt_start:
 gdt_null:
@@ -54,23 +54,51 @@ gdt_descriptoor:
 
 [BITS 32]
 load32:
-    mov ax, DATA_SEG
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov ebp, 0x00200000
-    mov esp, ebp
+    mov eax, 1
+    mov ecx, 100
+    mov edi, 0x0100000
+    call ata_lba_read
+    jmp CODE_SEG:0x0100000
 
-    ; Enable the A20 line
-    in al, 0x92
-    or al, 2
-    out 0x92, al
+ata_lba_read:
+    mov ebx, eax ; Backup the LBA
+    ; Send the highest 8 bits of the LBA to the hard disk controller
+    shr eax, 24
+    or eax, 0xe0 ; Select the master drive
+    mov dx, 0x1f6
+    out dx, al
+    ; Finished sending more bits of the LBA
 
-    jmp $
+    mov dx, 0x1f4
+    mov eax, ebx
+    shr eax, 8
+    out dx, al
+
+    mov eax, 0x1f5
+    mov eax, ebx
+    shr eax, 16
+    out dx, al
+
+    mov dx, 0x1f7
+    mov al, 0x20
+    out dx, al
+
+.next_sector:
+    push ecx
+
+.try_again:
+    mov dx, 0x1f7
+    in al, dx
+    test al, 8
+    jz .try_again
+
+    mov ecx, 256
+    mov dx, 0x1f0
+    rep insw
+    pop ecx
+    loop .next_sector
+    ; End reading sectors
+    ret
 
 times 510-($ - $$) db 0
 dw 0xAA55
-
-buffer:
